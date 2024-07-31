56d26fcf3d8aad24650a9cc19793e3dd1079d7b8
Tobias Gerschner <TobiG77@users.noreply.github.com>
2024-07-31 18:17:28 +1200

    devenv config for phoenix + elixir UTF8 locale

--- devenv.nix
+++ devenv.nix
@@ -1,27 +1,39 @@
-{ pkgs, lib, config, inputs, ... }:
+ { pkgs, ... }:
 
 {
+
+  env.LOCALE_ARCHIVE="$(nix profile list --json | jq -r '.elements.glibcLocales.storePaths[]')/lib/locale/locale-archive";
+  env.LANG = "en_NZ.UTF-8";
+  env.LANGUAGE = "en_NZ.UTF-8";
+  env.LC_ALL = "en_NZ.UTF-8";
+
   # https://devenv.sh/basics/
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [ pkgs.git pkgs.nodejs_20 pkgs.postgresql pkgs.inotify-tools pkgs.glibcLocales ];
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
+  enterTest = ''
+    echo "Ensuring LC_ALL is set to en_NZ.UTF-8";
+    test "$LC_ALL" = "en_NZ.UTF-8"
   '';
 
   enterShell = ''
+    elixir --version
+    echo "node version: `node --version`"
+  '';
+
+  languages = {
+    elixir = {
+      enable = true;
+      package = pkgs.beam.packages.erlangR26.elixir_1_16;
+    };
+  };
+
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_16;
+    listen_addresses = "0.0.0.0";
     hello
     git --version
   '';
@@ -37,3 +49,12 @@
 
   # See full reference at https://devenv.sh/reference/options/
 }
+      log_connections = true;
+      log_statement = "all";
+      logging_collector = true;
+      log_disconnections = true;
+      log_destination = "stderr";
+    };
+  };
+}
+
