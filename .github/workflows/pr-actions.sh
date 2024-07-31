5f36dcb32b2a889c73059dab5372e69523d5f86e
Tobias Gerschner <TobiG77@users.noreply.github.com>
2024-08-01 09:30:55 +1200

    tidy up

--- .github/workflows/pr-actions.sh
+++ .github/workflows/pr-actions.sh
@@ -4,14 +4,8 @@ set -euo pipefail
 
 # assumes `devenv up -d` has been run
 
-MIX_ENV=test
-
 cd hello_team_umbrella
 mix do deps.get, compile
 mix ecto.drop ||:
 mix ecto.setup
 mix test
-# cd apps/hello_team ; mix test
-# cd ../../
-
-# cd apps/hello_team_web ; mix test
