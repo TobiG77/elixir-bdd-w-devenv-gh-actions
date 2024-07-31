4bdbd66d136a472def59e062785cb8c3b27bd909
Tobias Gerschner <TobiG77@users.noreply.github.com>
2024-08-01 09:07:17 +1200

    minor tweaks & path fix

--- .github/workflows/generate-env-phoenix-test.sh
+++ .github/workflows/generate-env-phoenix-test.sh
@@ -2,6 +2,8 @@
 
 [ -e .env-phoenix-test ] || {
   cd hello_team_umbrella
+  mix local.hex --force
+  mix local.rebar --force
   MIX_PHX_GEN_SECRET=$(mix phx.gen.secret)
   echo test
   export MIX_PHX_GEN_SECRET
